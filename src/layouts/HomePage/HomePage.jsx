cdaf670027536146a31bde72c6f8d3116982b183
Christian Lara <christianramirezlara23@gmail.com>
2022-02-14 08:44:21 -0600

    Create and add the welcome layout

--- src/layouts/HomePage/HomePage.jsx
+++ src/layouts/HomePage/HomePage.jsx
@@ -1,7 +1,44 @@
+import Button from '../../components/Button/Button';
 import './HomePage.scss';
+import searchIcon from './Search-Icon.svg';
 
 const HomePage = () => {
-  return <p>Home Page!</p>;
+  return (
+    <section className="home-page">
+      <article className="home-page__welcome">
+        <div className="home-page__welcome__content">
+          <h3 className="home-page__welcome__content__title">
+            Welcome to YelpCamp!
+          </h3>
+          <p className="home-page__welcome__content__paragraph">
+            View our hand-picked campgrounds from all over the world, or add
+            your own.
+          </p>
+          <form className="home-page__welcome__content__form">
+            <div className="home-page__welcome__content__form__input-container">
+              <img
+                className="home-page__welcome__content__form__input-container__icon"
+                src={searchIcon}
+                alt="Magnifying glass"
+              />
+              <input
+                placeholder="Search for camps"
+                className="home-page__welcome__content__form__input-container__input"
+                type="text"
+              />
+            </div>
+            <Button textContent="Search" />
+          </form>
+          <a
+            className="home-page__welcome__content__add-campground-link"
+            href="#"
+          >
+            Or add your own campground
+          </a>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default HomePage;
