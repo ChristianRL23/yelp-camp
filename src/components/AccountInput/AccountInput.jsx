a8f9d831f682b8cb7c769daf877854100c415fbc
Christian Lara <christianramirezlara23@gmail.com>
2022-02-14 14:18:48 -0600

    Create AccountInput component.

---
+++ src/components/AccountInput/AccountInput.jsx
@@ -0,0 +1,19 @@
+import './AccountInput.scss';
+
+const AccountInput = (props) => {
+  return (
+    <div className="account-input">
+      <label className="account-input__label" htmlFor={props.label}>
+        {props.label}
+      </label>
+      <input
+        className="account-input__input"
+        id={props.label}
+        type="text"
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
+
+export default AccountInput;
