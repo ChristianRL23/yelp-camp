bd7595fab11648de03ef5d7693b052633581bd7c
Christian Lara <christianramirezlara23@gmail.com>
2022-02-13 18:08:58 -0600

    Add partners logos to the Landing layout.

--- src/layouts/LandingPage/LandingPage.jsx
+++ src/layouts/LandingPage/LandingPage.jsx
@@ -2,6 +2,9 @@ import Button from '../../components/Button/Button';
 import LandingFeature from '../../components/LandingFeature/LandingFeature';
 import './LandingPage.scss';
 import logo from './Logo.svg';
+import airbnbLogo from './Airbnb.svg';
+import bookingLogo from './Booking.svg';
+import plumGuideLogo from './Plum-Guide.svg';
 
 const LandingPage = () => {
   return (
@@ -24,6 +27,16 @@ const LandingPage = () => {
             <LandingFeature text="See locations for all camps." />
           </div>
           <Button textContent="View Campgrounds" />
+          <div className="landing-page__left__welcome-text__partners">
+            <h6 className="landing-page__left__welcome-text__partners__title">
+              Partnered with:
+            </h6>
+            <div className="landing-page__left__welcome-text__partners__logos">
+              <img src={airbnbLogo} alt="Partner logo" />
+              <img src={bookingLogo} alt="Partner logo" />
+              <img src={plumGuideLogo} alt="Partner logo" />
+            </div>
+          </div>
         </div>
       </div>
       <div className="landing-page__right" />
