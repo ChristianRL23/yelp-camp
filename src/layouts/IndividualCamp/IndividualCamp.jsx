0794c454ed4031346cf4310a7cffc29238c993ff
Christian Lara <christianramirezlara23@gmail.com>
2022-02-16 11:57:15 -0600

    Add map to the individual camp layout

--- src/layouts/IndividualCamp/IndividualCamp.jsx
+++ src/layouts/IndividualCamp/IndividualCamp.jsx
@@ -1,10 +1,17 @@
 import './IndividualCamp.scss';
 import Header from './../../components/Header/Header';
+import campMap from './Map.png';
 
 const IndividualCamp = () => {
   return (
     <section className="individual-camp">
       <Header />
+      <div className="individual-camp__container">
+        <aside className="individual-camp__container__sidebar">
+          <img src={campMap} alt="Camp location" />
+        </aside>
+        <main></main>
+      </div>
     </section>
   );
 };
