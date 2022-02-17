a46d1d974a5758fee583afa3c0f163b2a323d3bd
Christian Lara <christianramirezlara23@gmail.com>
2022-02-17 11:29:50 -0600

    Add Button for leave a review in the IndividualCamp layout.

--- src/layouts/IndividualCamp/IndividualCamp.jsx
+++ src/layouts/IndividualCamp/IndividualCamp.jsx
@@ -1,10 +1,12 @@
 import './IndividualCamp.scss';
 import Header from './../../components/Header/Header';
+import Footer from './../../components/Footer/Footer';
 import campMap from './Map.png';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import CampComment from '../../CampComment/CampComment';
+import Button from '../../components/Button/Button';
 
 const IndividualCamp = () => {
   const [campground, setCampground] = useState(null);
@@ -51,16 +53,24 @@ const IndividualCamp = () => {
                 Submited by: {campground.createdBy}
               </p>
             </div>
-            <div className="individual-camp__container__campground__comments">
-              {campground.comments.map((comment) => (
-                <CampComment
-                  author={comment.author}
-                  content={comment.content}
-                />
-              ))}
+            <div className="individual-camp__container__campground__reviews">
+              <div className="individual-camp__container__campground__reviews__comments">
+                {campground.comments.map((comment) => (
+                  <CampComment
+                    author={comment.author}
+                    content={comment.content}
+                  />
+                ))}
+              </div>
+              <Button
+                style={{ alignSelf: 'flex-end' }}
+                textContent="Leave a Review"
+                theme="black"
+              />
             </div>
           </main>
         </div>
+        <Footer />
       </section>
     )
   );
