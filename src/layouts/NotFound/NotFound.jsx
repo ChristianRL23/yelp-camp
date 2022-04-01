94d1b4e1440f76900b8b36585c6b1b294fdf2fa8
Christian Lara <christianramirezlara23@gmail.com>
2022-04-01 11:04:39 -0600

    Make responsive the NotFound layout.

--- src/layouts/NotFound/NotFound.jsx
+++ src/layouts/NotFound/NotFound.jsx
@@ -1,19 +1,24 @@
-import Footer from '../../components/Footer/Footer';
-import Header from '../../components/Header/Header';
+import { useNavigate } from 'react-router-dom';
+import Button from './../../components/Button/Button';
 import './NotFound.scss';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <section className="not-found">
-      <Header loggedStateHidden />
       <div className="not-found__content">
         <div className="not-found__content__texts">
           <h2>404</h2>
           <h3>Not Found</h3>
           <p>The resource requested could not be found!</p>
         </div>
+        <Button
+          clickFn={() => navigate('/home')}
+          theme="black"
+          textContent="Go to the homepage"
+        />
       </div>
-      <Footer />
     </section>
   );
 };
